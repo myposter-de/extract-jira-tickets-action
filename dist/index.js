346cbf336ef982ab9b85d84cc7d925bf52d93987
martin.berchtold <martin.berchtold@myposter.de>
2022-02-01 10:36:46 +0100

    chore: fix check

--- dist/index.js
+++ dist/index.js
@@ -61396,7 +61396,7 @@ async function extractJiraIssues() {
 
     let commits;
 
-    if (!!isPr) {
+    if (isPr === 'false') {
       console.log('rest.pulls.listCommits with number ', prNumber);
       const { data: commitsPulls } = await octokit.rest.pulls.listCommits({
         pull_number: prNumber,
